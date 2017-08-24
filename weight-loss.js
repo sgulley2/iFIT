7fe53b672c12fefc1ed135194c831ca503f74c46
Stuart Gulley <sgulley@msn.com>
2017-08-24 09:34:39 -0400

    Initial checkin

---
+++ weight-loss.js
@@ -0,0 +1,31 @@
+
+var $window, $animation_rows;
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_rows, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+$(document).ready(function() {
+  $window = $(window);
+  $animation_rows = $('tr');
+
+  $window.on('scroll resize', check_if_in_view);
+});
+
